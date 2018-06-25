5ecf3656f4fd50555287688d9b6e938a2a6affe0
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-06-25 23:15:38 +0900

    Testプロジェクトを .NET Core から .NET Framework のものに変更

---
+++ OpenWeatherClient.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenWeatherClient.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenWeatherClient.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("583d4f95-8a20-45d4-8ca0-cec7f31caf63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
